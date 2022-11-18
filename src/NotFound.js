87213de32f0a3bc34911c7cb04360e680778f1bd
jithya471 <jithya471@gmail.com>
2022-11-18 13:03:38 +0530

    final

--- src/NotFound.js
+++ src/NotFound.js
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
         <div className="mt-5 pt-5" style={{textAlign:"center"}}>
             <h1>Oops! You seem to be lost.</h1>
-            <p>Here are some helpful links:</p>
+            
            
         </div>
     )
